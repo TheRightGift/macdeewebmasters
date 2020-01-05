f3fe21e755dff2dc435cb15d41c37db68cd3e2bc
Chigozie Chukwu <goziechukwu@gmail.com>
2020-01-05 10:50:24 +0100

    first commit

---
+++ js/macdee.js
@@ -0,0 +1,31 @@
+M.AutoInit();
+
+$('.moreLess').on('click', function(e){
+    e.preventDefault();
+
+    let which = $(this).attr('id');
+    
+    if(which == 'moreLessPara' || which == 'moreLessPara2'){
+        let section = $(this).attr('data-section');
+        let hasHide = $('#'+section+' .moreLessContainer').hasClass('hide');
+
+        if(hasHide == true){
+            $('#'+section+' .moreLessContainer').removeClass('hide').addClass('show');
+            $('#'+section+' .moreLess').text('See Less');
+        } else if(hasHide == false){
+            $('#'+section+' .moreLessContainer').removeClass('show').addClass('hide');
+            $('#'+section+' .moreLess').text('See More');
+        }
+    } else if(which == 'moreLessList') {
+        let section = $(this).attr('data-section');
+        let hasHide = $('#'+section+' .contentToggle').hasClass('hide');
+        
+        if(hasHide == true){
+            $('#'+section+' .contentToggle').removeClass('hide').addClass('show');
+            $('#'+section+' .moreLess').html('<div class="collapsible-header centr">See Less</div>');
+        } else if(hasHide == false){
+            $('#'+section+' .contentToggle').removeClass('show').addClass('hide');
+            $('#'+section+' .moreLess').html('<div class="collapsible-header centr">27 More</div>');
+        }
+    }
+})
